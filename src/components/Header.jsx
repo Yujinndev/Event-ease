8d97e850de22daa1474c8203be7076adaa1075c8
Yujinndev <xyujinwho@gmail.com>
2024-03-17 01:22:56 +0800

    chore: continue building landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,50 +24,71 @@ function NavLink({ onToggle }) {
   )
 }
 
-function Actions() {
-  return (
-    <>
-      <Button variant="outline">Login</Button>
-      <Button>Create Account</Button>
-    </>
-  )
-}
-
 function Header() {
   const [isOpen, setOpen] = useState(false)
+  const [isAuth, setAuth] = useState(true)
 
   const handleToggleMenu = () => {
     setOpen(!isOpen)
   }
 
   return (
-    <header className="flex md:justify-around md:items-center md:px-4">
-      <div className="flex justify-between w-screen md:w-auto items-center py-3 md:p-0">
-        <div className="brand text-xl font-bold">EventEase</div>
-        <button className="md:hidden" onClick={() => handleToggleMenu()}>
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            ></path>
-          </svg>
-        </button>
+    <header className="z-50 flex md:items-center md:justify-between md:px-4">
+      <div className="flex w-screen items-center justify-between py-3 md:w-auto md:px-4">
+        <div className="brand text-xl font-bold text-green-700 lg:text-3xl">
+          SynchroFission
+        </div>
+
+        {isAuth === true ? (
+          <button className="md:hidden" onClick={() => handleToggleMenu()}>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {!isOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              )}
+            </svg>
+          </button>
+        ) : (
+          <Button size="sm" className="ms-4 px-6 md:hidden">
+            Sign in
+          </Button>
+        )}
       </div>
-      <div className="hidden md:flex gap-2">
-        <Actions />
-      </div>
-      {isOpen && (
-        <div className="mt-4 flex flex-col items-start absolute p-2 gap-4 top-[75%] w-full bg-slate-100 md:hidden">
+      {isAuth && (
+        <div className="hidden gap-2 md:flex">
           <NavLink onToggle={() => handleToggleMenu()} />
-          <Actions />
+        </div>
+      )}
+
+      <div className="hidden gap-2 md:flex">
+        <Button size="sm" className="mx-4 px-6">
+          Sign in
+        </Button>
+      </div>
+      {isAuth && isOpen && (
+        <div className="absolute top-[50%] mt-4 flex w-full flex-col items-start gap-4 bg-white p-1 py-4 md:hidden">
+          <NavLink onToggle={() => handleToggleMenu()} />
+
+          <Button variant="destructive" className="mx-4 px-6">
+            Logout
+          </Button>
         </div>
       )}
     </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,87 @@
 import React from "react"
+import logo from "@/assets/hero-image.png"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+
+const features = [
+  {
+    name: "FINANCE TRACKER & ANALYSIS",
+    desc: "Suggest date ideas and venues that align with the specified budget, whether it's a casual coffee date or a more elaborate dinner.",
+  },
+  {
+    name: "EVENT MANAGEMENT",
+    desc: "Understand the preferences of both individuals involved in the date, considering factors like favorite cuisines, preferred activities, and ambiance.",
+  },
+  {
+    name: "GUEST EMAIL INVITATION",
+    desc: "Invite guests in a modernized manner. Users can effortlessly input guest email addresses, customize invitations, and efficiently manage RSVPs.",
+  },
+  {
+    name: "TIMELINE & TASK MANAGEMENT",
+    desc: "Assist in planning the timeline for the date, from making reservations to organizing surprise elements, ensuring a smooth and enjoyable experience.",
+  },
+  {
+    name: "CALENDAR INTEGRATION",
+    desc: "Users can schedule tasks, set reminders, and synchronize event details with their personal calendars, ensuring organized planning and timely execution.",
+  },
+]
 
 function Home() {
   return (
-    <div className="hero-container">
-      <div>Hello</div>
+    <div className="overflow-x-hidden">
+      <div className="flex h-[100vh] w-[100vw] flex-col items-center justify-center gap-2 px-8 lg:flex-row">
+        <div className="h-64 scale-125 md:mt-16 md:scale-95 md:opacity-70 lg:hidden">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="relative flex w-full flex-col gap-4 md:mt-28 md:flex-auto md:text-left lg:w-3/5">
+          <h1 className="title brand text-4xl md:text-5xl lg:text-6xl">
+            Event Ease
+          </h1>
+          <p className="subtitle text-justify text-lg md:text-2xl lg:text-3xl">
+            Our ultimate event planner web app for any occasion, offering
+            finance budgeting, customizable event pages, a user-friendly
+            calendar, and geolocation integration. From birthdays to weddings,
+            effortlessly plan and manage every detail of your event with ease
+            and precision.
+          </p>
+          <div className="flex flex-col gap-4 md:w-64 md:flex-row">
+            <Button className="flex-auto">Try it out</Button>
+            <Button variant="outline">Explore</Button>
+          </div>
+        </div>
+        <div className="hidden h-96 w-full flex-col gap-4 md:w-1/2 md:flex-auto lg:flex">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+
+      <div className="flex h-fit w-[100vw] flex-col items-center justify-center gap-2 px-8 py-4">
+        <div className="absolute h-[215%] w-full bg-amber-800 md:h-4/5" />
+        <div className="brand z-10 mb-3 text-2xl md:text-5xl">
+          Our Features & Services
+        </div>
+        <div className="align-center z-10 flex flex-col flex-wrap justify-center gap-4 md:flex-row">
+          {features.map((el) => {
+            return (
+              <Card
+                key={el.name}
+                className="w-96 rounded-sm bg-white drop-shadow-sm hover:drop-shadow-xl"
+              >
+                <CardHeader>
+                  <img src={logo} alt="logo" />
+                </CardHeader>
+                <CardContent>
+                  <CardTitle className=" text-lg md:text-xl">
+                    {el.name}
+                  </CardTitle>
+                  <p className="subtitle text-justify text-lg lg:text-xl">
+                    {el.desc}
+                  </p>
+                </CardContent>
+              </Card>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
